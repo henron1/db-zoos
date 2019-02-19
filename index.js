eb08e5ddba9ecc212033220c7102f057ff931c86
Henry Neal <henry.neal0@gmail.com>
2019-02-19 16:01:55 -0500

    all crud operations firing

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const helmet = require('helmet');
 const knex = require('knex');
 
 const knexConfig = {
-  client: 'slqite3',
+  client: 'sqlite3',
   connection: {
-    filename: './data/zoos.db3',
+    filename: './data/lambda.sqlite3',
   },
   useNullAsDefault: true,
 };
@@ -59,7 +59,40 @@ server.post('/api/zoos', async (req, res) => {
   }
 });
 
+// Update // Update // Update // Update // Update // Update // Update // Update // Update // Update // Update // Update // Update // Update // Update 
+server.put('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id })
+      .update(req.body);
 
+    if (count > 0) {
+      const zoo = await db('zoos')
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({ message: 'Records not found' });
+    }
+  } catch (error) {}
+});
+
+// Delete // Delete // Delete // Delete // Delete // Delete // Delete // Delete // Delete // Delete // Delete // Delete // Delete // Delete // Delete 
+
+server.delete('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Records not found' });
+    }
+  } catch (error) {}
+});
 
 const port = 3300;
 server.listen(port, function() {
