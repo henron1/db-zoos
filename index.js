19103ead90325b010f71b272ee41c951f8b61b48
Henry Neal <henry.neal0@gmail.com>
2019-02-19 15:50:09 -0500

    added post and both get endpoints

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'slqite3',
+  connection: {
+    filename: './data/zoos.db3',
+  },
+  useNullAsDefault: true,
+};
+const db = knex(knexConfig);
 
 const server = express();
 
@@ -7,6 +17,49 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+// GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET 
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos');
+    res.status(200).json(zoos)
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.get('/api/zoos/:id', async (req, res) => {
+  // get the roles from the database
+  try {
+    const zoo = await db('zoos')
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(zoo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+const errors = {
+  '19' : 'another record with that value exists'
+}
+
+//Post //Post //Post //Post //Post //Post //Post //Post //Post //Post //Post //Post //Post //Post //Post //Post //Post //Post //Post //Post //Post 
+server.post('/api/zoos', async (req, res) => {
+  try {
+    const [id] = await db('zoos').insert(req.body);
+
+    const zoo = await db('zoos')
+      .where({ id })
+      .first();
+
+    res.status(201).json(zoo);
+  } catch (error) {
+    const message = errors[error.errno] || 'We ran into an error';
+    res.status(500).json({ message, error });
+  }
+});
+
+
 
 const port = 3300;
 server.listen(port, function() {
